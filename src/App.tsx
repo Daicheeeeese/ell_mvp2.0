20a50adbc5c6c2c90fb13842771429e5e06e66d0
Daicheeeeese <daicheese7@gmail.com>
2025-02-23 21:49:33 +0900

    Fix: Remove react-router-dom and use Next.js routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,10 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AppProps } from 'next/app';
 import { AuthProvider } from './contexts/AuthContext';
-import LoginPage from './pages/LoginPage';
-// ... 他のインポート
 
-function App() {
+function App({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
-      <Router>
-        <Routes>
-          <Route path="/login" element={<LoginPage />} />
-          {/* ... 他のルート */}
-        </Routes>
-      </Router>
+      <Component {...pageProps} />
     </AuthProvider>
   );
 }
